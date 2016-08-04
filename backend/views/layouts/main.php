5debb1a6b1aa55d48e1ec218e666efec59586172
nbarazi <nawar.a.albarazi@gmail.com>
2016-08-04 23:24:40 +0300

    Add RTL support

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -4,13 +4,17 @@
 /* @var $content string */
 
 use backend\assets\AppAsset;
+use backend\assets\AppAssetRTL;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
 
-AppAsset::register($this);
+if(Yii::$app->language == 'ar')
+  AppAssetRTL::register($this);
+else
+  AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
